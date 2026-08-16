b4db4c11bced6b3d094885454e58a1936f7b8746
naseemgit89 <naseem0507@gmail.com>
2026-08-17 01:04:47 +0530

    Update resource group configuration in main.tf
    Replaced existing resource group and storage account with a new resource group configuration.

--- main.tf
+++ main.tf
@@ -1,26 +1,10 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.81.0"
-    }
-  }
- 
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "mama1" {
-  name     = "rg-dev"
-  location = "East US"
-}
 
-resource "azurerm_storage_account" "mama" {
-  name                     = "storage989unt10854509n"
-  resource_group_name      = azurerm_resource_group.mama.name
-  location                 = azurerm_resource_group.mama.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_resource_group" "rg" {
+  name= "rg-naseem1"
+  location = "east us"
+  
 }
